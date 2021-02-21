4c397fbfde4a5c85a7142459b289c61b1c00aac7
Clement Ng <clement.ng@dynata.com>
2021-02-20 21:02:05 -0800

    bruh, send, receive, delete messages from sqs

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/golag-sqs
+
+go 1.15
+
+require (
+	github.com/aws/aws-sdk-go v1.37.15
+	github.com/kr/pretty v0.2.1
+)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/kr/pretty"
+)
+
+func main() {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("clement_AWS_REGION")),
+		//Credentials: credentials.NewStaticCredentials(os.Getenv("clement_AWS_ACCESS_KEY_ID"), os.Getenv("clement_AWS_SECRET_ACCESS_KEY"), ""),
+		MaxRetries: aws.Int(4),
+	})
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("failed to create session")
+		panic("fail to crate session")
+	}
+	svc := sqs.New(sess)
+
+	// send the message
+	sendParam := &sqs.SendMessageInput{
+		MessageBody:  aws.String("here is the message body"),
+		QueueUrl:     aws.String(os.Getenv("clement_AWS_QUEUE")),
+		DelaySeconds: aws.Int64(3),
+	}
+	sendResponse, err := svc.SendMessage(sendParam)
+	if err != nil {
+		fmt.Println("=========")
+		fmt.Println(err)
+		panic("failed to send message")
+	}
+	pretty.Println("sendming message: ", sendResponse)
+
+	//receive thr message
+	receiveParam := &sqs.ReceiveMessageInput{
+		QueueUrl:            aws.String(os.Getenv("clement_AWS_QUEUE")),
+		MaxNumberOfMessages: aws.Int64(5),
+		VisibilityTimeout:   aws.Int64(30),
+		WaitTimeSeconds:     aws.Int64(20),
+	}
+	receiveResponse, err := svc.ReceiveMessage(receiveParam)
+	if err != nil {
+		fmt.Println(err)
+		panic("failed to receive mesage")
+	}
+	pretty.Println("receiving message: ", receiveResponse)
+
+	//delete the mesasge from sqs
+	for _, message := range receiveResponse.Messages {
+		deleteParam := &sqs.DeleteMessageInput{
+			QueueUrl:      aws.String(os.Getenv("clement_AWS_QUEUE")),
+			ReceiptHandle: message.ReceiptHandle,
+		}
+		_, err := svc.DeleteMessage(deleteParam)
+		if err != nil {
+			fmt.Println("failed to delete message from SQS queue")
+			log.Println(err)
+		}
+		pretty.Println("Deleted message ID: ", *message.MessageId)
+	}
+}
